67ab7902cdc39b316c3ebf60d019bbe5c3815c40
Jinniepoo <118496261+Jinniepoo@users.noreply.github.com>
2025-08-17 21:05:18 +0900

    Update Texture.h

--- EngineSDK/Inc/Texture.h
+++ EngineSDK/Inc/Texture.h
@@ -2,9 +2,6 @@
 
 #include "Component.h"
 
-/* 셰이더에 텍스쳐의 정보를 던져서 셰이더내에서 이 텍스쳐 픽셀의 색을 픽셀에 입히는 용도로 사용하기위한 테긋쳐 집합. */
-/* 픽셀의 색을 입히는 용도로 셰이더던지고 쌤플링해온다. */
-
 BEGIN(Engine)
 
 class ENGINE_DLL CTexture final : public CComponent
@@ -24,7 +21,6 @@ public:
 	HRESULT Bind_ShaderResources(class CShader* pShader, const _char* pConstantName);
 
 private:
-	// ID3D11Texture2D -> ID3D11ShaderResourceView, ID3D11RenderTargetView, ID3D11DepthStencilView 
 	vector<ID3D11ShaderResourceView*>				m_SRVs;
 	_uint											m_iNumTextures = { 0 };
 
