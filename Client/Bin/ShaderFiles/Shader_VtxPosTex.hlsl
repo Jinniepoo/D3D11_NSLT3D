1c050a2fcd40a10ae77c5540f3b5687f2053cd0a
Jinniepoo <118496261+Jinniepoo@users.noreply.github.com>
2025-08-17 21:40:44 +0900

    Update Shader_VtxPosTex.hlsl

--- Client/Bin/ShaderFiles/Shader_VtxPosTex.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxPosTex.hlsl
@@ -1,13 +1,9 @@
 
 #include "Engine_Shader_Defines.hlsli"
-// vector == float4 float3 float2 float
-// matrix == float4x4 
 
-/* 상수테이블. == 전역변수 */
 float4x4 g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
 texture2D g_Texture;
 
-/* 디퍼드 세ㅜ이더르 수행하면서 빛연산이 필요한 픽셀들의 깊이를 저장해놓았다. */
 texture2D g_DepthTexture;
 
 struct VS_IN
@@ -22,13 +18,6 @@ struct VS_OUT
     float2 vTexcoord : TEXCOORD0;
 };
 
-// 리턴값 VS_MAIN(float3	vPosition : POSITION, float2 vTexcoord : TEXCOORD)
-/* IA : VertexBuffer(4) + IndexBuffer(6 -> 012023) */
-/* SV_ : Shader Value : 내가 셰이더에서 처리해야할 모든 연산을 끝냈으니 앞으로는 니가 알아서 해야할 과정을 수행해라. */
-
-/* VertexShader */
-/* 1. 정점의 기본적인 변환(월드, 뷰, 투영)을 수행한다.: 로컬스페이스상의 정점정보를 투영행렬까지 곱해서 처리해주는 과정이 필요하다 . */
-/* 2. 정점의 구성을 변경한다.  */
 VS_OUT VS_MAIN(VS_IN In)
 {
     VS_OUT Out = (VS_OUT) 0;
@@ -65,17 +54,10 @@ VS_OUT_EFFECT VS_MAIN_EFFECT(VS_IN In)
     return Out;
 }
 
-/* Triangle List로 그리고 있는 경우, 정점 세개가 정점 쉐이더를 통과할 때까지 모으는 과정을 거치낟. */
-/* w나누기 연산을 통해서 투영에 해당하는 과정을 거치낟. 정점들은 투영스페이스(-1, 1 ~ 1, -1) 상에 존재하낟. */
-/* 뷰포트 스페이스로 변환한다. 정점들은 윈도우 좌표공간(0, 0 ~ winsizex, winsizey)에 존재한다.  */
-/* 래스라이즈를 수행하게된다. : 정점 정보를 보간하여 픽셀의 정보를 생성한다. */
-/* 생성된 픽셀을 픽셀 쉐이더로 던져서 픽셀의 색을 결정한다. */
-
 struct PS_IN
 {
     float4 vPosition : SV_POSITION;
     float2 vTexcoord : TEXCOORD0;
-
 };
 
 struct PS_OUT
@@ -83,15 +65,12 @@ struct PS_OUT
     float4 vColor : SV_TARGET0;
 };
 
-/* 픽셀의 색을 결정하낟. */
 PS_OUT PS_MAIN(PS_IN In)
 {
     PS_OUT Out = (PS_OUT) 0;
 
-	// Out.vColor = g_Texture.Sample(어떤방식으로 가져올건데?, 어디에 색을 가져올건데?);
     Out.vColor = g_Texture.Sample(LinearSampler, In.vTexcoord);
 
-	/* 알파테스트 더 이상 렌더스테이츠로 설정한다(x) */
     if (Out.vColor.a <= 0.1f)
         discard;
 
@@ -109,14 +88,8 @@ struct PS_IN_EFFECT
 PS_OUT PS_MAIN_EFFECT(PS_IN_EFFECT In)
 {
     PS_OUT Out = (PS_OUT) 0;
-
-	// Out.vColor = g_Texture.Sample(어떤방식으로 가져올건데?, 어디에 색을 가져올건데?);
     Out.vColor = g_Texture.Sample(LinearSampler, In.vTexcoord);
 
-	/* 두개의 깊이. (내 픽셀의 깊이, 이 픽셀을 그릴려고 했던 위치에 이미 그려져있던 픽셀의 깊이 */
-	/*In.vProjPos.z => 0 ~ f
-	In.vProjPos.w => n ~ f
-	In.vProjPos.z / In.vProjPow.w = > 0 ~ 1 보간된 깊이를 선형으로 저장하는 연산의 방법. */
     float fViewZ = In.vProjPos.w;
 
     float2 vDepthUV;
@@ -132,9 +105,6 @@ PS_OUT PS_MAIN_EFFECT(PS_IN_EFFECT In)
 
 technique11 DefaultTechnique
 {
-	/* Pass : 셰이더 기능의 캡슐화 (여러기법을 모아놓은) */
-
-	/* 빛연산 + 림라이트 + 그림자 + 메탈릭 */
     pass Default
     {
         SetRasterizerState(RS_Default);
