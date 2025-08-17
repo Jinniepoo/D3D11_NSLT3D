e6dbd72c9db0555c79f818ddfb92e2a4e4ae430d
Jinniepoo <118496261+Jinniepoo@users.noreply.github.com>
2025-08-17 21:39:26 +0900

    Update Shader_VtxNorTex.hlsl

--- Client/Bin/ShaderFiles/Shader_VtxNorTex.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxNorTex.hlsl
@@ -26,13 +26,6 @@ struct VS_OUT
     float4 vProjPos : TEXCOORD2;
 };
 
-// 리턴값 VS_MAIN(float3	vPosition : POSITION, float2 vTexcoord : TEXCOORD)
-/* IA : VertexBuffer(4) + IndexBuffer(6 -> 012023) */
-/* SV_ : Shader Value : 내가 셰이더에서 처리해야할 모든 연산을 끝냈으니 앞으로는 니가 알아서 해야할 과정을 수행해라. */
-
-/* VertexShader */
-/* 1. 정점의 기본적인 변환(월드, 뷰, 투영)을 수행한다.: 로컬스페이스상의 정점정보를 투영행렬까지 곱해서 처리해주는 과정이 필요하다 . */
-/* 2. 정점의 구성을 변경한다.  */
 VS_OUT VS_MAIN(VS_IN In)
 {
     VS_OUT Out = (VS_OUT) 0;
@@ -49,12 +42,6 @@ VS_OUT VS_MAIN(VS_IN In)
 
     return Out;
 }
-
-/* Triangle List로 그리고 있는 경우, 정점 세개가 정점 쉐이더를 통과할 때까지 모으는 과정을 거치낟. */
-/* w나누기 연산을 통해서 투영에 해당하는 과정을 거치낟. 정점들은 투영스페이스(-1, 1 ~ 1, -1) 상에 존재하낟. */
-/* 뷰포트 스페이스로 변환한다. 정점들은 윈도우 좌표공간(0, 0 ~ winsizex, winsizey)에 존재한다.  */
-/* 래스라이즈를 수행하게된다. : 정점 정보를 보간하여 픽셀의 정보를 생성한다. */
-/* 생성된 픽셀을 픽셀 쉐이더로 던져서 픽셀의 색을 결정한다. */
 
 struct PS_IN
 {
@@ -73,12 +60,10 @@ struct PS_OUT
     float4 vPickedDepth : SV_TARGET3;
 };
 
-/* 픽셀의 색을 결정하낟. */
 PS_OUT PS_MAIN(PS_IN In)
 {
     PS_OUT Out = (PS_OUT) 0;
 
-	// Out.vColor = g_Texture.Sample(어떤방식으로 가져올건데?, 어디에 색을 가져올건데?);
     vector vSourDiffuse = g_DiffuseTexture[0].Sample(LinearSampler, In.vTexcoord * 30.f);
     vector vDestDiffuse = g_DiffuseTexture[1].Sample(LinearSampler, In.vTexcoord * 30.f);
 
@@ -112,9 +97,6 @@ PS_OUT PS_MAIN(PS_IN In)
 
 technique11 DefaultTechnique
 {
-	/* Pass : 셰이더 기능의 캡슐화 (여러기법을 모아놓은) */
-
-	/* 빛연산 + 림라이트 + 그림자 + 메탈릭 */
     pass Default
     {
 		// SetRasterizerState(RS_Wireframe);
